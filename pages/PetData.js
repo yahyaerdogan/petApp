632b602316dc49ba5d2f9c02802cce2940f9aa4e
yahyaerdogan <yahyaerdogan1199@gmail.com>
2021-05-03 22:09:11 +0300

    New data file created and related actions

---
+++ pages/PetData.js
@@ -0,0 +1,29 @@
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "586194a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "bd7a2cbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "Fourth Item",
+  },
+  {
+    id: "3ac68af3c-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Fifth Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Sixth Item",
+  }
+];
+
+export {DATA};
